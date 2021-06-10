064cbb87f63254855afb0965de222622088adc2b
minenash <minenash@protonmail.com>
2021-06-10 01:01:11 -0400

    Fixed clients with the mod on servers without it

--- src/main/java/com/minenash/monsters_in_the_closet/mixin/BedBlockMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/BedBlockMixin.java
@@ -1,19 +1,15 @@
-package net.fabricmc.example.mixin;
+package com.minenash.monsters_in_the_closet.mixin;
 
-import net.fabricmc.example.MonstersInTheCloset;
+import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.enums.BedPart;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
--- src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayerEntityMixin.java
@@ -1,10 +1,9 @@
-package net.fabricmc.example.mixin;
+package com.minenash.monsters_in_the_closet.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.example.MonstersInTheCloset;
+import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.mob.HostileEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
