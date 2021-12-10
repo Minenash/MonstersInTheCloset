76221415310aa979460519220fc240eaef89c6e7
solonovamax <solonovamax@12oclockpoint.com>
2021-12-10 16:41:38 -0500

    Update to 1.18

--- src/main/java/com/minenash/monsters_in_the_closet/MonstersInTheCloset.java
+++ src/main/java/com/minenash/monsters_in_the_closet/MonstersInTheCloset.java
@@ -1,18 +1,15 @@
 package com.minenash.monsters_in_the_closet;
 
+
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MonstersInTheCloset {
-
-    // Server Side
-    public static World world = null;
-
     // Client Side
     public static int duration = -1;
+    
     public static List<HostileEntity> list = new ArrayList<>();
-
 }
--- src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayerEntityMixin.java
@@ -1,8 +1,9 @@
 package com.minenash.monsters_in_the_closet.mixin;
 
+
+import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,19 +12,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 
+
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
-
+    
     @Inject(method = "tick", at = @At("HEAD"))
-    public void tick(CallbackInfo  _info) {
-
+    public void tick(CallbackInfo info) {
+        
         if (MonstersInTheCloset.duration == 0)
             MonstersInTheCloset.list = new ArrayList<>();
-
+        
         if (MonstersInTheCloset.duration >= 0)
             MonstersInTheCloset.duration--;
-
+        
     }
-
+    
 }
