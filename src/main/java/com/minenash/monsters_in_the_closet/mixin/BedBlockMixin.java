6c241350cf6251af839366db2e60c1312fc8783b
minenash <minenash@protonmail.com>
2022-03-04 20:33:38 -0500

    Revert "Merge pull request #5 from solonovamax/1.18"
    This reverts commit f18fa87eca47662d6d64f3b0e542ab4e40cfa72b, reversing
    changes made to fecbb2a186976ef3eb07f9b44fda6174b8c37cb9.

--- src/main/java/com/minenash/monsters_in_the_closet/mixin/BedBlockMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/BedBlockMixin.java
@@ -1,6 +1,5 @@
 package com.minenash.monsters_in_the_closet.mixin;
 
-
 import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
@@ -22,42 +21,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
-    
-    @Unique
-    private static BlockPos blockPos = null;
-    
-    @Inject(method = "method_19283",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;sendMessage(Lnet/minecraft/text/Text;Z)V"))
-    private static void thingy(PlayerEntity player, PlayerEntity.SleepFailureReason reason, CallbackInfo info) {
-        if (reason != PlayerEntity.SleepFailureReason.NOT_SAFE)
-            return;
-        
-        
-        if (blockPos != null) {
-            Vec3d vec3d = Vec3d.ofBottomCenter(blockPos);
-            List<HostileEntity> list = player.world.getEntitiesByClass(
-                    HostileEntity.class,
-                    new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D,
-                            vec3d.getZ() + 8.0D),
-                    (hostileEntity) -> hostileEntity.isAngryAt(player)
-                                                                      );
-            
-            if (!list.isEmpty()) {
-                MonstersInTheCloset.duration = 60;
-                MonstersInTheCloset.list = list;
-            }
-        }
-        
-        
-    }
-    
-    @Inject(method = "onUse", at = @At("HEAD"))
-    public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult result,
-                      CallbackInfoReturnable<ActionResult> info) {
-        blockPos = pos;
-    }
-    
+
+	@Unique
+	private static BlockPos blockPos;
+
+	@Inject(method = "onUse", at = @At("HEAD"))
+	public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand _hand, BlockHitResult _result, CallbackInfoReturnable<ActionResult> _info) {
+		blockPos = pos;
+		if (!world.isClient())
+			MonstersInTheCloset.world = world;
+	}
+
+	@Inject(method = "method_19283", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;sendMessage(Lnet/minecraft/text/Text;Z)V"))
+	private static void thingy(PlayerEntity player, PlayerEntity.SleepFailureReason reason, CallbackInfo info) {
+		if (reason != PlayerEntity.SleepFailureReason.NOT_SAFE)
+			return;
+
+		Vec3d vec3d = Vec3d.ofBottomCenter(blockPos);
+		List<HostileEntity> list = player.world.getEntitiesByClass(
+				HostileEntity.class,
+				new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D, vec3d.getZ() + 8.0D),
+				(hostileEntity) -> hostileEntity.isAngryAt(player)
+		);
+
+		if (!list.isEmpty()) {
+			MonstersInTheCloset.duration = 60;
+			MonstersInTheCloset.list = list;
+		}
+
+
+	}
+
 }
--- src/main/java/com/minenash/monsters_in_the_closet/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/ServerPlayerEntityMixin.java
@@ -1,11 +1,9 @@
 package com.minenash.monsters_in_the_closet.mixin;
 
-
 import com.mojang.datafixers.util.Either;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
+import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
@@ -24,30 +22,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.List;
 import java.util.Optional;
 
-
 @Environment(EnvType.SERVER)
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends Entity {
-    public ServerPlayerEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
-    
+public class ServerPlayerEntityMixin {
+
+
     @Inject(method = "trySleep", at = @At("RETURN"))
     public void highlightMobs(BlockPos pos, CallbackInfoReturnable<Either<PlayerEntity.SleepFailureReason, Unit>> info) {
+        World world = MonstersInTheCloset.world;
         Optional<PlayerEntity.SleepFailureReason> reason = info.getReturnValue().left();
         if (world != null && reason.isPresent() && reason.get() == PlayerEntity.SleepFailureReason.NOT_SAFE) {
-            
+
             Vec3d vec3d = Vec3d.ofBottomCenter(pos);
             List<HostileEntity> list = world.getEntitiesByClass(
                     HostileEntity.class,
-                    new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D,
-                            vec3d.getZ() + 8.0D),
+                    new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D, vec3d.getZ() + 8.0D),
                     (hostileEntity) -> hostileEntity.isAngryAt((PlayerEntity) (Object) this)
-                                                               );
-            
+            );
+
             for (HostileEntity entity : list)
                 entity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 60, 0, true, false));
-            
+
         }
     }
+
 }
