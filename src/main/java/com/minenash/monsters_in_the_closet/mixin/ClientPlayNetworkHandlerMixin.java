4f17e8a6aa24d39e21946f6961d0ebdb0cd85450
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-28 11:33:12 +0300

    Change if statement

--- src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayNetworkHandlerMixin.java
@@ -3,11 +3,11 @@ package com.minenash.monsters_in_the_closet.mixin;
 
 import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.message.MessageHandler;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.network.MessageType;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
@@ -22,7 +22,7 @@ import java.util.List;
 
 @Mixin(MessageHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-    
+
     @Shadow
     @Final
     private MinecraftClient client;
@@ -35,21 +35,18 @@ public class ClientPlayNetworkHandlerMixin {
         )
     )
     private void interceptDangerousSleepMessage(Text message, boolean overlay, CallbackInfo ci) {
-        if (packet.getType() == MessageType.GAME_INFO && packet.getMessage() instanceof TranslatableText &&
-            "block.minecraft.bed.not_safe".equals(((TranslatableText) packet.getMessage()).getKey())) {
-            Vec3d vec3d = Vec3d.ofBottomCenter(client.player.getBlockPos());
-            List<HostileEntity> list = client.world.getEntitiesByClass(
-                    HostileEntity.class,
-                    new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D,
-                            vec3d.getZ() + 8.0D),
-                    hostileEntity -> hostileEntity.isAngryAt(client.player)
-                                                                      );
-            
-            if (!list.isEmpty()) {
-                MonstersInTheCloset.duration = 60;
-                MonstersInTheCloset.list = list;
-            }
+        if (!(message.getContent() instanceof TranslatableTextContent && message.getString().equals("block.minecraft.bed.not_safe"))) return;
+
+        Vec3d vec3d = Vec3d.ofBottomCenter(client.player.getBlockPos());
+        List<HostileEntity> list = client.world.getEntitiesByClass(
+            HostileEntity.class,
+            new Box(vec3d.getX() - 8.0D, vec3d.getY() - 5.0D, vec3d.getZ() - 8.0D, vec3d.getX() + 8.0D, vec3d.getY() + 5.0D,
+                vec3d.getZ() + 8.0D),
+            hostileEntity -> hostileEntity.isAngryAt(client.player));
+
+        if (!list.isEmpty()) {
+            MonstersInTheCloset.duration = 60;
+            MonstersInTheCloset.list = list;
         }
     }
-    
 }
