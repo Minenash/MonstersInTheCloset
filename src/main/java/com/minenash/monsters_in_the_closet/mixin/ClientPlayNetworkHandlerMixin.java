d2d472cdd5529e32096cadbe5396c446bf22fdf4
solonovamax <solonovamax@12oclockpoint.com>
2022-10-07 15:27:52 -0400

    Update for 1.19.2

--- src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/minenash/monsters_in_the_closet/mixin/ClientPlayNetworkHandlerMixin.java
@@ -5,9 +5,8 @@ import com.minenash.monsters_in_the_closet.MonstersInTheCloset;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.network.MessageType;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
@@ -30,11 +29,11 @@ public class ClientPlayNetworkHandlerMixin {
     @Inject(method = "onGameMessage",
             at = @At(value = "INVOKE",
                      shift = At.Shift.BEFORE,
-                     target = "Lnet/minecraft/client/gui/hud/InGameHud;addChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Ljava/util/UUID;)V")
+                     target = "Lnet/minecraft/client/network/message/MessageHandler;onGameMessage(Lnet/minecraft/text/Text;Z)V")
     )
     private void interceptDangerousSleepMessage(GameMessageS2CPacket packet, CallbackInfo ci) {
-        if (packet.getType() == MessageType.GAME_INFO && packet.getMessage() instanceof TranslatableText &&
-            "block.minecraft.bed.not_safe".equals(((TranslatableText) packet.getMessage()).getKey())) {
+        if (packet.content().getContent() instanceof TranslatableTextContent translatableText &&
+            "block.minecraft.bed.not_safe".equals(translatableText.getKey())) {
             Vec3d vec3d = Vec3d.ofBottomCenter(client.player.getBlockPos());
             List<HostileEntity> list = client.world.getEntitiesByClass(
                     HostileEntity.class,
